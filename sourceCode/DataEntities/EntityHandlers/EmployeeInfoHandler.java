dc2fca48939980823f81db01c0964dc92887257b
TimFries <tim.fries000@gmail.com>
2018-07-24 15:12:50 -0500

    Alter Code
    Added functionality to employeeCreationController.
    Added employee and user creation functionality to EmployeeInfoHandler
    Added TODO list in main root folder

--- sourceCode/DataEntities/EntityHandlers/EmployeeInfoHandler.java
+++ sourceCode/DataEntities/EntityHandlers/EmployeeInfoHandler.java
@@ -6,6 +6,10 @@
 package DataEntities.EntityHandlers;
 
 import DataEntities.EmployeeInfo;
+import Shared.DBUtil;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +25,29 @@ public class EmployeeInfoHandler{
     private static final String LOGIN_TABLE = "logininfo";
     
     public static void create(EmployeeInfo data){
+        String sql = "INSERT into " +EMPLOYEE_TABLE+ " (firstName, lastName, emailAddress,"
+                                                   + " streetAddress, city, zipcode, state, contactPhone)"
+                                                   + " VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try(
+                Connection  conn = DBUtil.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ){
+            stmt.setString(1, data.getFirstName());
+            stmt.setString(2, data.getLastName());
+            stmt.setString(3, data.getEmailAddress());
+            stmt.setString(4, data.getStreetAddress());
+            stmt.setString(5, data.getCity());
+            stmt.setInt(6, data.getZipcode());
+            stmt.setString(7, data.getState());
+            stmt.setString(8, data.getContactPhone());
+            stmt.execute();
+            
+            
+            
+        }
+        catch(SQLException e){
+            DBUtil.processException(e);
+        }
         
     }
     
@@ -28,24 +55,46 @@ public class EmployeeInfoHandler{
         
     }
     
-    public static List<EmployeeInfo> selectAll(){
+    public static List<EmployeeInfo> select(){
        List<EmployeeInfo> employeeList = new ArrayList<>();
        
        return employeeList;
     }
     
-    public static EmployeeInfo selectOne(Integer employeeID){
+    
+    public static EmployeeInfo select(Integer employeeID){
         EmployeeInfo employeeData = null;
         
         return employeeData;
     }
     
+    
     public static void delete(Integer employeeID){
         
     }
     
-    public static void createUser(Integer employeeID, String username, String password, String permissions){
-        
+    public static void createUser(String username, String password, String permissions, EmployeeInfo employeeData){
+        String sql = "INSERT into " + LOGIN_TABLE+ " (employeeID, userName, password, permissions) "
+                                                   + " VALUES ((SELECT employeeID FROM " + EMPLOYEE_TABLE
+                                                   + " WHERE firstName = ? AND lastName = ? AND emailAddress = ?), ?, MD5(?), ?)";
+        try(
+                Connection  conn = DBUtil.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ){
+            
+            stmt.setString(1, employeeData.getFirstName());
+            stmt.setString(2, employeeData.getLastName());
+            stmt.setString(3, employeeData.getEmailAddress());
+            stmt.setString(4, username);
+            stmt.setString(5, password);
+            stmt.setString(6, permissions);
+            stmt.execute();
+            
+            
+        }
+        catch(SQLException e){
+            DBUtil.processException(e);
+        }
     }
     
     public static void changePassword(Integer employeeID){
--- sourceCode/inventorymanagment/Controller_Files/EmployeeCreationController.java
+++ sourceCode/inventorymanagment/Controller_Files/EmployeeCreationController.java
@@ -5,15 +5,25 @@
  */
 package inventorymanagment.Controller_Files;
 
+import DataEntities.EmployeeInfo;
+import DataEntities.EntityHandlers.EmployeeInfoHandler;
 import Shared.ComboBoxDefaults;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -51,30 +61,95 @@ public class EmployeeCreationController implements Initializable {
     private ChoiceBox buildingPermChoice;
     @FXML
     private ChoiceBox itemsPermChoice;
+    @FXML
+    private Button submitButton;
     private String randomUsernameNum;
     
-    @FXML
-    private void handleUsernameCreation(){
-        
-        
-    }
-    
-    
+ 
     
     @FXML
     private void handleOnlyNumbers(KeyEvent ke){
         
-        if(!Character.isDigit(ke.getCharacter().charAt(0)) || phoneField.getText().trim().length()>9){
+        if((!Character.isDigit(ke.getCharacter().charAt(0)) || phoneField.getText().trim().length()>9) && ke.getSource().equals(phoneField)){
             ke.consume();
-            return;
+           
         }
+        else if((!Character.isDigit(ke.getCharacter().charAt(0)) || zipcodeField.getText().trim().length()>4) && ke.getSource().equals(zipcodeField)){
+            ke.consume();
+        }
+        
         //TODO format when phoneField has reached 10 digits long
         
     }
     
     @FXML
-    private void handleEmployeeCreation(){
+    private void handleFieldFormat(KeyEvent ke){
         
+        
+        if (ke.getSource().equals(lastNameField) || ke.getSource().equals(firstNameField)) {
+            if ((!Character.isLetter(ke.getCharacter().charAt(0)) || lastNameField.getText().trim().length() > 15 || firstNameField.getText().trim().length() > 15)) {
+                ke.consume();
+            }
+
+        }
+        else if(ke.getSource().equals(addressField)){
+            if((!Character.isLetterOrDigit(ke.getCharacter().charAt(0)) && !Character.isWhitespace(ke.getCharacter().charAt(0))) || addressField.getText().trim().length() > 63){
+                ke.consume();
+            }
+            
+        }
+        else if(ke.getSource().equals(cityField)){
+            if((!Character.isLetter(ke.getCharacter().charAt(0)) && !Character.isWhitespace(ke.getCharacter().charAt(0))) || cityField.getText().trim().length() > 31){
+                ke.consume();
+            }
+        }
+        else if(ke.getSource().equals(emailField)){
+            if(addressField.getText().trim().length() > 254){
+                ke.consume();
+            }
+        }
+        
+
+
+    }
+    
+    @FXML
+    private void handleUsernameGen(){
+        if(!firstNameField.getText().isEmpty() && !lastNameField.getText().isEmpty()){
+            alterUsername();
+        }
+    }
+    @FXML
+    private void handleEmployeeCreation(){
+        if(formCheck()){
+           
+            if(checkFormPatterns()){
+                EmployeeInfo employeeData = new EmployeeInfo();
+                employeeData.setFirstName(firstNameField.getText().trim().toLowerCase());
+                employeeData.setLastName(lastNameField.getText().trim().toLowerCase());
+                employeeData.setEmailAddress(emailField.getText().trim());
+                employeeData.setStreetAddress(addressField.getText().trim().toLowerCase());
+                employeeData.setCity(cityField.getText().trim().toLowerCase());
+                employeeData.setZipcode(Integer.valueOf(zipcodeField.getText().trim()));
+                employeeData.setState(stateChoice.getSelectionModel().getSelectedItem().toString());
+                employeeData.setContactPhone(String.format("(%s) %s-%s", 
+                                                                        phoneField.getText().trim().substring(0, 3),
+                                                                        phoneField.getText().trim().substring(3, 6),
+                                                                        phoneField.getText().trim().substring(6, 10)));
+                //TODO change SQL create methods to boolean for error checking
+                EmployeeInfoHandler.create(employeeData);
+                EmployeeInfoHandler.createUser(usernameDisplay.getText().trim().toLowerCase(), tempPasswordDisplay.getText().trim(), permissionString(), employeeData);
+                Stage stage = (Stage) submitButton.getScene().getWindow();
+                stage.close();
+                
+            }
+            else{
+                System.out.println("Email didn't match pattern");
+            }
+        }
+        else{
+            System.out.println("Something failed");
+        }
     }
     
     @FXML
@@ -88,6 +163,8 @@ public class EmployeeCreationController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         setChoiceBoxs();
+        setUsernameNum();
+        generateTempPassword();
         
     }    
     
@@ -103,8 +180,158 @@ public class EmployeeCreationController implements Initializable {
         employeePermChoice.getSelectionModel().select(0);
         buildingPermChoice.getSelectionModel().select(1);
         itemsPermChoice.getSelectionModel().select(1);
-        
+ 
                 
+    }
+    
+    private void setUsernameNum(){
+        Random rand = new Random();
+        randomUsernameNum = String.valueOf(rand.nextInt(9));
+        randomUsernameNum += String.valueOf(rand.nextInt(9));
+    }
+    
+    private void generateTempPassword(){
+        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lower = upper.toLowerCase();
+        String digits = "0123456789";
+        String alphanum = upper+digits+lower;
+        String randomPass = "";
+        Random rand = new Random();
+        for(int i = 0; i < 6; i++){
+            randomPass += alphanum.charAt(rand.nextInt(alphanum.length()));
+        }
+        tempPasswordDisplay.setText(randomPass);
+    }
+    
+    private void alterUsername(){
+        String lastTemp;
+
+        
+        lastTemp = lastNameField.getText();
+        if(lastTemp.length() > 5){
+             lastTemp = lastTemp.substring(0, 5);
+        }
+            usernameDisplay.setText(String.format("%s%c%s", lastTemp.toLowerCase(), firstNameField.getText().toLowerCase().charAt(0), randomUsernameNum));
+    }
+    
+    private boolean formCheck(){
+        boolean pass = true;
+        
+        
+        if(firstNameField.getText().isEmpty()){
+            pass = false;
+            firstNameField.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            firstNameField.setStyle("-fx-text-box-border: black;");
+        }
+        if(lastNameField.getText().isEmpty()){
+            pass = false;
+            lastNameField.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            lastNameField.setStyle("-fx-text-box-border: black;");
+        }
+        if(addressField.getText().isEmpty()){
+            pass = false;
+            addressField.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            addressField.setStyle("-fx-text-box-border: black;");
+        }
+        if(cityField.getText().isEmpty()){
+            pass = false;
+            cityField.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            cityField.setStyle("-fx-text-box-border: black;");
+        }
+        if(stateChoice.getSelectionModel().isEmpty()){
+            pass = false;
+            stateChoice.setStyle("-fx-border-color: red;");
+        }
+        else{
+            stateChoice.setStyle("-fx-border-color: black;");
+        }
+        if(zipcodeField.getText().isEmpty()){
+            pass = false;
+            zipcodeField.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            zipcodeField.setStyle("-fx-text-box-border: black;");
+        }
+        if(emailField.getText().isEmpty()){
+            pass = false;
+            emailField.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            emailField.setStyle("-fx-text-box-border: black;");
+        }
+        if(phoneField.getText().isEmpty()){
+            pass = false;
+            phoneField.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            phoneField.setStyle("-fx-text-box-border: black;");
+        }
+        if(usernameDisplay.getText().isEmpty()){
+            pass = false;
+            usernameDisplay.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            usernameDisplay.setStyle("-fx-text-box-border: black;");
+        }
+        if(tempPasswordDisplay.getText().isEmpty()){
+            pass = false;
+            tempPasswordDisplay.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            tempPasswordDisplay.setStyle("-fx-text-box-border: black;");
+        }
+        
+        
+        
+        return pass;
+    }
+    
+    private boolean checkFormPatterns(){
+        boolean pass = true;
+        Pattern EMAIL_PATTERN = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        
+        Matcher matcher = EMAIL_PATTERN.matcher(emailField.getText().trim());
+        if(!matcher.find()){
+            pass = false;
+            emailField.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            emailField.setStyle("-fx-text-box-border: black;");
+        }
+        if(phoneField.getText().trim().length() != 10){
+            pass = false;
+            phoneField.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            phoneField.setStyle("-fx-text-box-border: black;");
+        }
+        if(zipcodeField.getText().trim().length() != 5){
+            pass = false;
+            zipcodeField.setStyle("-fx-text-box-border: red;");
+        }
+        else{
+            zipcodeField.setStyle("-fx-text-box-border: black;");
+        }
+        
+        return pass;
+    }
+    
+    private String permissionString(){
+        
+        
+        String temp = String.format("%d%d%d%d", adminPermChoice.getSelectionModel().getSelectedIndex(),
+                                         employeePermChoice.getSelectionModel().getSelectedIndex(),
+                                         buildingPermChoice.getSelectionModel().getSelectedIndex(),
+                                         itemsPermChoice.getSelectionModel().getSelectedIndex());
+        return temp;
     }
     
     
